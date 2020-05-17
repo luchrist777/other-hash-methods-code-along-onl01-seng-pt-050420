002ed71c85dd7f09035277a33d908ff131fb08f4
Luis Leandro <luchrist777@gmail.com>
2020-05-17 06:17:45 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,8 +1,13 @@
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
-      end
-    end
-  end
+require 'pry'
+
+#This is the groceries hash we'll be passing in to the get_the_values method
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"]
+# }
+
+def get_the_values(groceries)
+  groceries.values.flatten
+end
